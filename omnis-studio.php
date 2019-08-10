2d5b27dfeb5ca0143cdcde6fc5279adad1d56740
Adrian Lampart <yeroo.s3@gmail.com>
2019-08-10 17:25:46 +0200

    UPDATE added images-lazy and categories on front

--- omnis-studio.php
+++ omnis-studio.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Omnis Studio
  * Plugin URI:        loprd.pl
  * Description:       Omnis Studio plugin.
- * Version:           0.4
+ * Version:           0.41
  * Author:            Adrian Lampart
  * Author URI:        loprd.pl
  * License:           GPL-2.0+
