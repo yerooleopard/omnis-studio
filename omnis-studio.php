4176e10bc9eb61e80b6cd97493ed8173f59658c9
Adrian Lampart <yeroo.s3@gmail.com>
2019-09-03 14:00:10 +0200

    UPDATE new components

--- omnis-studio.php
+++ omnis-studio.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Omnis Studio
  * Plugin URI:        loprd.pl
  * Description:       Omnis Studio plugin.
- * Version:           0.44
+ * Version:           0.5
  * Author:            Adrian Lampart
  * Author URI:        loprd.pl
  * License:           GPL-2.0+
